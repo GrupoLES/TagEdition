f2e44d8f84ec2d86f3555ae74fa1d746c2e01c4e
italotm <italotm.19@gmail.com>
2013-09-03 05:45:39 -0300

    Remove funcionando!

--- src/br/com/activity/RemoveActivity.java
+++ src/br/com/activity/RemoveActivity.java
@@ -1,16 +1,68 @@
 package br.com.activity;
 
-import com.example.tagedition.R;
-import android.os.Bundle;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+import br.com.logica.AdapterCheckList;
+import br.com.logica.FileAdapter;
+import br.com.logica.ListMusic;
+
+import com.beaglebuddy.mp3.MP3;
+import com.example.tagedition.R;
 
 public class RemoveActivity extends Activity {
-
+	
+	private Button botaoRemoverSelecionados;
+	private Button botaoRemoverTudo;
+	private AdapterCheckList adapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_remove);
+		
+		final ListView listView = (ListView) findViewById(R.id.listRemove);
+		botaoRemoverSelecionados = (Button) findViewById(R.id.button1);
+		botaoRemoverTudo = (Button) findViewById(R.id.button2);
+		
+		adapter = new AdapterCheckList(this, ListMusic.getInstance().getListMusic());
+		listView.setAdapter(adapter);
+		
+		botaoRemoverSelecionados.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				List<File> aux = adapter.getSelecionados();
+				
+				for (File file : aux) {
+					ListMusic.getInstance().getListMusic().remove(file);
+				}
+				MainActivity.fileAdapter.notifyDataSetChanged();
+				finish();
+			}
+		});
+		
+		botaoRemoverTudo.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				List<File> list = ListMusic.getInstance().getListMusic();
+				list.removeAll(list);
+				MainActivity.fileAdapter.notifyDataSetChanged();
+				finish();
+			}
+		});
 	}
 
 	@Override
@@ -19,5 +71,4 @@ public class RemoveActivity extends Activity {
 		getMenuInflater().inflate(R.menu.remove, menu);
 		return true;
 	}
-
 }
---
+++ src/br/com/logica/AdapterCheckList.java
@@ -0,0 +1,88 @@
+package br.com.logica;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.activity.MainActivity;
+import br.com.activity.SetValueTag;
+
+import com.example.tagedition.R;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class AdapterCheckList extends BaseAdapter{
+	
+	private List<File> files;
+	private LayoutInflater mInflater;
+	private List<File> selecionados;
+	
+	public AdapterCheckList(Context context, List<File> files) {
+		mInflater = LayoutInflater.from(context);
+		this.files = files;
+		this.selecionados = new ArrayList<File>();
+	}
+	
+	@Override
+	public int getCount() {
+		return files.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		return files.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		return arg0;
+	}
+
+	@Override
+	public View getView(final int posicao, View view, ViewGroup arg2) {
+		view = mInflater.inflate(R.layout.item_check_list, null); 
+		
+		ImageView image = (ImageView)view.findViewById(R.id.tipoArquivo);
+		if (this.files.get(posicao).isDirectory()){
+			image.setImageResource(R.drawable.folder_blue_music);
+		}else{
+			image.setImageResource(R.drawable.music_icon);
+		}
+		
+		
+		TextView nome = (TextView)view.findViewById(R.id.nomeArquivo);
+		nome.setText(files.get(posicao).getName());
+		
+		final CheckBox check = (CheckBox)view.findViewById(R.id.checkBox);
+		
+		check.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (check.isChecked()){
+					System.out.println("entrou");
+					check.setButtonDrawable(R.drawable.com_facebook_button_check_on);
+					selecionados.add(files.get(posicao));
+				}else{
+					System.out.println("entrou2");				
+					check.setButtonDrawable(R.drawable.com_facebook_button_check_off);
+					selecionados.remove(files.get(posicao));
+				}
+			}
+		});
+		
+		return view;
+	}
+	
+	public List<File> getSelecionados(){
+		return selecionados;
+	}
+}
