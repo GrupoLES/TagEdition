60a4beb4f74237884518909af45bc96f2111a050
italotm <italotm.19@gmail.com>
2013-09-03 00:46:44 -0300

    nova activity remove

---
+++ src/br/com/activity/RemoveActivity.java
@@ -0,0 +1,26 @@
+package br.com.activity;
+
+import com.example.tagedition.R;
+import com.example.tagedition.R.layout;
+import com.example.tagedition.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class RemoveActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_remove);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.remove, menu);
+		return true;
+	}
+
+}
